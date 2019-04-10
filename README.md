e0928f461ceb3f5f242f1ef867910d3207e5d3b8
Emil Gedda <emil.gedda@infomaker.se>
2019-04-10 15:22:51 +0200

    build windows client

--- README.md
+++ README.md
@@ -1,16 +1,4 @@
-# Grävmaskin
-To be clear: **THIS IS A HACK** 
-
-This project is a result of the gräv hackathon arranged by infomaker 2019.
-
-[https://www.infomaker.se/news/infomaker-hackathon-pa-grav-2019/](https://www.infomaker.se/news/infomaker-hackathon-pa-grav-2019/)
-
-## To build and run 
+# To build and run 
 ```
 ./gradlew clean assemble run
-```
-
-##To build applications:
-```
-./gradlew clean build
 ```
--- src/main/kotlin/se/infomaker/actapublica/app/MyApp.kt
+++ src/main/kotlin/se/infomaker/actapublica/app/MyApp.kt
@@ -1,6 +1,19 @@
 package se.infomaker.actapublica.app
 
 import se.infomaker.actapublica.view.MainView
-import tornadofx.App
+import tornadofx.*
 
-class MyApp: App(MainView::class, Styles::class)
+class MyApp: App(MainView::class, Styles::class) {
+
+    companion object {
+        @JvmStatic
+        fun main(args : Array<String>) {
+            MyApp.launchApp(args)
+        }
+        @JvmStatic
+        fun launchApp(args: Array<String>) {
+            launch<MyApp>(args)
+        }
+    }
+
+}
